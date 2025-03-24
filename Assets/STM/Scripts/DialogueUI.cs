4b3f08100bd48793479d1020482adf3d0fdc81ee
D07\D07 <whdkdy02@naver.com>
2025-03-24 15:52:35 +0900

    DialogueTableLoader 생성 DialogueUI 에 대화를 띄우기 위해 함수 만들어야함 주석처리 보고 진행하기

--- Assets/STM/Scripts/DialogueUI.cs
+++ Assets/STM/Scripts/DialogueUI.cs
@@ -19,7 +19,13 @@ namespace AYO
 
         public void ShowDialogue()
         {
-            
+            // DialogueTableLoader 에서 기능을 가져와서 text에 집어넣기
+            // 엔터를 치면 다음 대사로 넘어가도록
+            // npc와 상호작용을 하면 lineID를 가져올 수 있도록 -> 불러오기?
+            // npc가 lineID를 가지고 있어야함
+            // NpcData 생성 (ScriptableObject 로 만들기) -> 상호작용하면 Data(string lineID) 읽어오기
+            // 읽어온 lineID를 DialogueTableLoader.GetDialogueData(string lineID) 에 넣고 string 불러오기
+            // 불러온 string 을 text로 변환
         }
 
         // Update is called once per frame
