5c91c6b24b85e5036c04838f15b0d6ff788a9985
D07\D07 <whdkdy02@naver.com>
2025-03-26 15:45:22 +0900

    DialManager 를 이용 > 엔터를 누르면 UI 에 대사 띄우기 구현

--- Assets/AYO/Scripts/DialManager.cs
+++ Assets/AYO/Scripts/DialManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +7,8 @@ namespace AYO
     public class DialManager : MonoBehaviour
     {
         private NPCData npcData;
-        private string currentLineID;
+        private string currentLineID = "LN010100";
+        private int i = -1; // i를 계속 초기화 해줘야함
 
         [SerializeField] private DialogueTableLoader tableLoader;
         [SerializeField] private Text dialogueLine;
@@ -16,19 +16,36 @@ namespace AYO
         // Start is called before the first frame update
         void Start()
         {
-        
+            
         }
 
+        // npcData = GetComponent<NPCData>();  >> 어떻게 가져올것인지?
+        // lines의 첫번째 줄을 저장하는 변수를 만들 필요 X !!
+        // 반복문을 사용하는 대신 함수에 i++ & Enter 를 누를때마다 실행되도록
         public void ShowDialogue()
         {
             // npcData = GetComponent<NPCData>();  >> 어떻게 가져올것인지?
             List<string> lines = tableLoader.GetDialogueData(currentLineID);
+            if(i < lines.Count - 1)
+            {
+                i++;
+                dialogueLine.text = lines[i];
+            }
+        }
+
+        public void StartDialogue()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Debug.Log("입력");
+                ShowDialogue();
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            StartDialogue();
         }
     }
 }
